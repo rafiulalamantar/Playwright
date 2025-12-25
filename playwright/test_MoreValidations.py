7082d97266d9ebed64b02aca0e34a26412fe7259
Rafiul Alam Antar <53433694+rafiulalamantar@users.noreply.github.com>
2025-12-25 16:43:15 +0600

    hidden and visibility in python playwright

---
+++ playwright/test_MoreValidations.py
@@ -0,0 +1,8 @@
+from playwright.sync_api import Page, expect
+
+
+def test_UIChecks(page: Page):
+    page.goto("https://rahulshettyacademy.com/AutomationPractice/")
+    expect(page.get_by_placeholder("Hide/Show Example")).to_be_visible()
+    page.get_by_role("button",name = "Hide").click()
+    expect(page.get_by_placeholder("Hide/Show Example")).to_be_hidden()
