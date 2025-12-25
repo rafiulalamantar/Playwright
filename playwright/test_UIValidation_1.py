5d26cfe5a505aac9fb76b305deaf5c8ed567b602
Rafiul Alam Antar <53433694+rafiulalamantar@users.noreply.github.com>
2025-12-25 16:14:09 +0600

    How to handle child windows and tabs with playwright

--- playwright/test_UIValidation_1.py
+++ playwright/test_UIValidation_1.py
@@ -4,15 +4,27 @@ from playwright.sync_api import Page, expect
 def test_UIValidationDynamicScript(page: Page):
     # iPhone X, Nokia Edge -> Verify 2 items are showing in Cart
 
-    page.goto("")
-    page.get_by_label("Username:").fill("")
+    page.goto("https://example.com")
+
+    page.get_by_label("Username:").fill("rahul")
     page.get_by_label("Password:").fill("learning")
     page.get_by_role("combobox").select_option("teach")
     page.locator("#terms").check()
     page.get_by_role("button", name="Sign In").click()
+
     iphone_product = page.locator("app-card").filter(has_text="iPhone X")
     iphone_product.get_by_role("button").click()
+
     nokia_product = page.locator("app-card").filter(has_text="Nokia Edge")
     nokia_product.get_by_role("button").click()
+
     page.get_by_text("Checkout").click()
     expect(page.locator(".media-body")).to_have_count(2)
+
+def test_childWindowPage(page: Page):
+    page.goto("https://rahulshettyacademy.com/loginpagePractise/")
+    with page.expect_popup() as newPage_info:
+        page.locator(".blinkingText").click()
+        childPage = newPage_info.value
+        text = childPage.locator(".red").text_content()
+        print(text)
