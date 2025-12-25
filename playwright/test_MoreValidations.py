22e6dadbeda7b5df8ff2f17b5c2df927e1a451c9
Rafiul Alam Antar <53433694+rafiulalamantar@users.noreply.github.com>
2025-12-25 16:53:53 +0600

    Handle alerts with anonymous function lamda

--- playwright/test_MoreValidations.py
+++ playwright/test_MoreValidations.py
@@ -1,3 +1,5 @@
+import time
+
 from playwright.sync_api import Page, expect
 
 
@@ -6,3 +8,8 @@ def test_UIChecks(page: Page):
     expect(page.get_by_placeholder("Hide/Show Example")).to_be_visible()
     page.get_by_role("button",name = "Hide").click()
     expect(page.get_by_placeholder("Hide/Show Example")).to_be_hidden()
+
+    #Alert Boxes
+    page.on("dialog", lambda dialog: dialog.accept())
+    page.get_by_role("button",name = "Confirm").click()
+    time.sleep(4)
