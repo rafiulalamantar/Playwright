28fd01ca03b84cca7a405f9f043f48793d8c64d0
Rafiul Alam Antar <53433694+rafiulalamantar@users.noreply.github.com>
2026-01-10 23:15:00 +0600

    change syntax on python function

--- playwright/test_NetworkOne.py
+++ playwright/test_NetworkOne.py
@@ -1,9 +1,26 @@
 from playwright.sync_api import Page
 
 
+fakePayloadResponse = {
+    "data": [],
+    "message": "No Orders"
+}
+
+def intercept_response(route):
+    route.fulfill(
+        json=fakePayloadResponse
+    )
+
 def test_NetworkOne(page: Page):
+    page.route(
+        "https://rahulshettyacademy.com/api/ecom/order/get-orders-for-customer/*",
+        intercept_response
+    )
     page.goto("https://rahulshettyacademy.com/client/")
     page.get_by_placeholder("email@example.com").fill("rahulshetty@gmail.com")
     page.get_by_placeholder("enter your passsword").fill("Iamking@000")
-    page.get_by_role('button', name= "Login").click()
-    page.get_by_role('button', name= "ORDERS").click()
+    page.get_by_role("button", name="Login").click()
+    page.get_by_role("button", name="ORDERS").click()
+
+    order_text = page.locator(".mt-4").text_content()
+    print(order_text)
