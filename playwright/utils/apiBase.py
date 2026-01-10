dcb91c49e06237e14c56d4afd3cfa4fa9c8eb87e
Rafiul Alam Antar <53433694+rafiulalamantar@users.noreply.github.com>
2026-01-10 12:30:11 +0600

    api post call with new function and print response json in console

--- playwright/utils/apiBase.py
+++ playwright/utils/apiBase.py
@@ -1,6 +1,9 @@
 from playwright.sync_api import Playwright
 
-
+ordersPayload = {{"orders":[{"country":"British Indian Ocean Territory","productOrderedId":"6960eac0c941646b7a8b3e68"}]}}
 class APIUtils:
 
     def createOrder(self, playwright: Playwright):
+        api_request_context = playwright.request.new_context(base_url="https://rahulshettyacademy.com")
+        response = api_request_context.post("/api/ecom/order/create-order", data=ordersPayload, headers = {"Authorization" : token, "Content-Type" : "application/json"})
+        print(response.json())
