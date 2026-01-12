f494008019dd80d03fbeeab0ae18bce47d5db3c9
Rafiul Alam Antar <53433694+rafiulalamantar@users.noreply.github.com>
2026-01-12 22:12:04 +0600

    from json convert to python object then use data parametrize pytest annotation by there calling from the custom fixture

--- playwright/pageObjects/LoginPage.py
+++ playwright/pageObjects/LoginPage.py
@@ -3,5 +3,11 @@ class LoginPage:
         self.page = page
 
 
-    def navigate(self, page):
-        page.goto("https://rahulshettyacademy.com/client/")
+    def navigate(self):
+        self.page.goto("https://rahulshettyacademy.com/client/")
+
+
+    def login(self,userEmail,userPassword):
+        self.page.get_by_placeholder("email@example.com").fill(userEmail)
+        self.page.get_by_placeholder("enter your passsword").fill(userPassword)
+        self.page.get_by_role('button', name="Login").click()
--- playwright/test_framework_web_api.py
+++ playwright/test_framework_web_api.py
@@ -13,7 +13,12 @@ with open('playwright/data/credential.json') as f:
 
 
 @pytest.mark.parametrize('user_credentials', userCredentials_list)
+
 def test_web_api(playwright: Playwright, user_credentials):
+
+    userEmail = user_credentials["userName"]
+    password = user_credentials["userPassword"]
+
     browser = playwright.chromium.launch(headless=False)
     context = browser.new_context()
     page = context.new_page()
@@ -25,9 +30,8 @@ def test_web_api(playwright: Playwright, user_credentials):
 
     #Login to the System
     loginPage = LoginPage(page)
-    page.get_by_placeholder("email@example.com").fill(user_credentials["userEmail"])
-    page.get_by_placeholder("enter your passsword").fill(user_credentials["userPassword"])
-    page.get_by_role('button', name= "Login").click()
+    loginPage.navigate()
+    loginPage.login(userEmail, password)
     page.get_by_role('button', name= "ORDERS").click()
 
     row = page.locator("tr").filter(has_text=orderId)
