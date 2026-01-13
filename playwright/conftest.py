c5e26425fd375fc11b02e4f52fdf71051dc1f51b
Rafiul Alam Antar <53433694+rafiulalamantar@users.noreply.github.com>
2026-01-13 21:12:22 +0600

    Passing different values to a test function, depending on command line options

--- playwright/conftest.py
+++ playwright/conftest.py
@@ -1,13 +1,19 @@
 import pytest
+from pytest_playwright.pytest_playwright import browser_name
+
 
 @pytest.fixture(scope="session")
 def userCredentials(request):
     return request.param
 
+def pytest_addoption(parser):
+    parser.addoption(
+        "--browser_name", action="store", default="chrome", help="browser selection"
+    )
 
 @pytest.fixture(scope="session")
 def browserInstance(playwright,request):
-    request.config.getoption("--browser")
+    browser_name = request.config.getoption("browser_name")
     browser = playwright.chromium.launch(headless=False)
     context = browser.new_context()
     page = context.new_page()
